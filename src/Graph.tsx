dc755c87add1561e5753c1808cd8b6b130e57f4d
DRKorossy <david.roland.korossy@gmail.com>
2023-08-26 17:14:30 +0100

    Created a better graph for the trader

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective'; // TableData wasn't imported
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -23,11 +23,17 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
-    };
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
+    }; // previously we plotted the two stocks to compare them, see how much the
+       // top_ask_price is over time for each. But the trader would like to see what the ratio
+       // is between the two. So in this step, we added all the attributes that we'll need
+       // to the object called 'schema' - and we will use this later for plotting.
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
@@ -35,26 +41,37 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('view', 'y_line'); // we want a y-plot
+      //removed the column pivots, because we no longer need to plot two graphs
+      elem.setAttribute('row-pivots', '["timestamp"]'); // this is our x-axis
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+      //these are the things we will plot, and we don't need any more data. this will
+      //make the plot clear and easy to interpret, and removes unnecessary data, or noise.
+
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
-      }));
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
+        })
+      ); // these are the attributes of the plot
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ]as unknown as TableData);
     }
   }
-}
+} // this makes sure that our graph updates correctly
+                                  // when it gets new data
 
 export default Graph;
+
+// We created a graph that plots the ratio between two stocks
+// and it shows when a trading opportunity arises!
